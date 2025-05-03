c2890df2b30138c2c8a55c7ee4a8f9b774e4f7b9
gn-pmcgeough <pierce.mcgeough@nbcuni.com>
2025-05-03 23:57:10 +0100

    WIP

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -2,14 +2,11 @@
 
 namespace App\Models;
 
-use App\Models\Role;
-use App\Models\Task;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
-    /** @use HasFactory<\Database\Factories\PeopleFactory> */
     use HasFactory;
 
     public function role()
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,14 +2,12 @@
 
 namespace App\Models;
 
-use App\Models\Person;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Role extends Model
 {
-    /** @use HasFactory<\Database\Factories\RolesFactory> */
     use HasFactory;
 
     public function people(): HasMany
