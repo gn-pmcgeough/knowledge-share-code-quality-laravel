5acb03b6eddc79c3a5a469eb6a6d78b5d14a4712
gn-pmcgeough <pierce.mcgeough@nbcuni.com>
2025-05-03 18:43:49 +0100

    WIP

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -5,16 +5,35 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
     </head>
     <body>
-        <h1>Task List</h1>
+        <div class="container">
+            <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+                <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                    <span class="fs-4">Knowledge Share</span>
+                </a>
+            </header>
 
-        <ul>
-            @foreach ($tasks as $task)
-                <li>
-                    {{ $task->name }} - {{ $task->person->name }} ({{ $task->person->role->name }})
-                </li>
-            @endforeach
-        </ul>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Task</th>
+                        <th scope="col">Person</th>
+                        <th scope="col">Role</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($tasks as $task)
+                        <tr>
+                            <td>{{ $task->name }}</td>
+                            <td>{{ $task->person->name }}</td>
+                            <td>{{ $task->person->role->name }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </body>
 </html>
